ddb74bbac3a30ec88b34d1619de4d2fe39f31601
Haimcer <allan1995.ars@gmail.com>
2022-12-14 14:20:33 -0300

    Tela pedidos

---
+++ lib/src/pages/orders/components/order_tile.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:greengrosser/src/models/order_model.dart';
+import 'package:greengrosser/src/services/utils_services.dart';
+
+import '../../../models/cart_item_model.dart';
+
+class OrderTile extends StatelessWidget {
+  OrderTile({super.key, required this.order});
+
+  final OrderModel order;
+
+  final UtilsServices utilsServices = UtilsServices();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      elevation: 1,
+      child: Theme(
+        data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+        child: ExpansionTile(
+          childrenPadding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+          title: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Pedido: ${order.id}'),
+              Text(
+                utilsServices.formatDateTime(order.createdDateTime),
+                style: const TextStyle(
+                  fontSize: 12,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+          children: [
+            SizedBox(
+              height: 150,
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: ListView(
+                      children: order.items.map((orderItem) {
+                        return _OrderItemWidget(
+                          utilsServices: utilsServices,
+                          orderItem: orderItem,
+                        );
+                      }).toList(),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      color: Colors.blue,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _OrderItemWidget extends StatelessWidget {
+  const _OrderItemWidget({
+    Key? key,
+    required this.utilsServices,
+    required this.orderItem,
+  }) : super(key: key);
+
+  final UtilsServices utilsServices;
+  final CartItemModel orderItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Row(
+        children: [
+          Text(
+            '${orderItem.quantity} ${orderItem.item.unit} ',
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Expanded(child: Text(orderItem.item.itemName)),
+          Text(utilsServices.priceToCurrency(orderItem.totalPrice())),
+        ],
+      ),
+    );
+  }
+}
